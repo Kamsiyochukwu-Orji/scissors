4ff324854e52f60d4803841dee9ff49a48b33313
Kamsiyochukwu-Orji <kamsiyochukwuorji@yahoo.com>
2026-06-10 13:42:31 +0100

    fix:wrapped the ErrorBoundary around other components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { HomePage } from './pages/HomePage'
-import { DashboardPage } from './pages/DashboardPage'
-import { ExpiredPage } from './pages/ExpiredPage'
-import { RedirectPage } from './pages/RedirectPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { DashboardPage } from "./pages/DashboardPage";
+import { ExpiredPage } from "./pages/ExpiredPage";
+import { RedirectPage } from "./pages/RedirectPage";
+import { ErrorBoundary } from "./components/ErrorBoundary";
+import { ToastContainer } from "./components/Toast";
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/expired" element={<ExpiredPage />} />
-        <Route path="/:slug" element={<RedirectPage />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <ErrorBoundary>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/expired" element={<ExpiredPage />} />
+          <Route path="/:slug" element={<RedirectPage />} />
+        </Routes>
+        <ToastContainer />
+      </BrowserRouter>
+    </ErrorBoundary>
+  );
 }
 
-export default App
+export default App;
